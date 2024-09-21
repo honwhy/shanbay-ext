527680bd326b11ce504c0016fee8e3fc5b513d84
honwhy.wang <honwhy.wang@hotmail.com>
2024-09-22 01:42:19 +0800

    feat: popup

--- src/entrypoints/popup/App.tsx
+++ src/entrypoints/popup/App.tsx
@@ -1,6 +1,8 @@
 import { Button } from '@/components/ui/button'
+import { useEffect, useState } from 'react'
 
 import { ExAction } from '../types'
+import './style.css'
 
 interface ExCookie {
   expirationDate: number
@@ -18,14 +20,32 @@ function App() {
         }
       })
   }, [])
-
+  const handleBatchAdd = () => {
+    browser.tabs.create({ url: 'https://web.shanbay.com/wordsweb/#/collection' })
+  }
+  const handleBeginLearning = () => {
+    browser.tabs.create({ url: 'https://web.shanbay.com/wordsweb/#/collection' })
+  }
+  const handleLogin = () => {
+    browser.tabs.create({ url: 'https://web.shanbay.com/web/account/login/' })
+  }
   return (
-    <>
-      <Button>
-        popup
-        {isLogin ? '已登录' : '未登录'}
-      </Button>
-    </>
+    <div>
+      <h2>扇贝查词插件</h2>
+      <ul>
+        <li><a href="https://github.com/honwhy/shanbay-ext">关于插件</a></li>
+        {isLogin
+        && (
+          <>
+            <li><button id="batch-add" onClick={handleBatchAdd}>批量添加单词</button></li>
+            <li><button id="begin-learning" onClick={handleBeginLearning}>开始背单词</button></li>
+            <li><button id="options">插件设置</button></li>
+            <li><button id="logout">退出</button></li>
+          </>
+        )}
+        {!isLogin && <li><button id="login" onClick={handleLogin}>登录</button></li>}
+      </ul>
+    </div>
   )
 }
 
